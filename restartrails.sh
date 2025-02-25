679e139d53af3b4ad2aed73308fee6556c1d8936
Emilyann Kerr <6244757+emilyannk@users.noreply.github.com>
2025-02-25 11:43:34 -0500

    shoo

--- restartrails.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-docker compose -f docker-compose-sirenia.yml exec -w /app/samvera/hyrax-webapp web bundle exec rails restart
--- start.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-docker compose -f docker-compose-sirenia.yml up -d && docker compose -f docker-compose-sirenia.yml logs -f
